8eaa5afd056cbf7db5cdd24b9319adc3e6380cbd
Blas-U <cheezypizzagamer@gmail.com>
2021-05-11 10:16:18 -0500

    ggs

--- loops4.py
+++ loops4.py
@@ -8,3 +8,7 @@ pixels = neopixel.NeoPixel(board.D18, 20)
 for i range (20,0):
     pixels[i] = (255, 69, 0)
     time.sleep (.25)
+
+for x in range (1):
+    pixels[x] = (255, 69, 0)
+    time.sleep (.25)
