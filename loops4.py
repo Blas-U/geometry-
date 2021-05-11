665e8985469495710bdc0ce0b141daba22cabac0
Blas-U <cheezypizzagamer@gmail.com>
2021-05-11 10:18:16 -0500

    ggs

--- loops4.py
+++ loops4.py
@@ -9,6 +9,6 @@ for i in range (20,0):
     pixels[i] = (255, 69, 0)
     time.sleep (.25)
 
-for x in range (1):
-    pixels[x] = (255, 69, 0)
-    time.sleep (.25)
+#for x in range (1):
+#    pixels[x] = (255, 69, 0)
+#    time.sleep (.25)
